5c06c14b15b6033251ddd9c34d628f8e33548af4
fazia idiri <fidiri3120@students.mc3.eu>
2022-03-03 13:56:36 -0500

    part3

--- src/App.js
+++ src/App.js
@@ -1,54 +1,38 @@
-import React, {useEffect, useState} from 'react';
-import {formatStudentData} from './utils'
-import './App.css';
-
-const URL = "https://api.hatchways.io/assessment/students";
-
-function App() {
-
-  const [studentData, setStudentData] = useState([]); 
-
-  const getStudents = (URL) => {
-     fetch(URL)
-      .then(response => response.json())
-      .then(data => formatStudentData(data.students))
-      .then(formatted => setStudentData(formatted))
-      .catch(error => alert('Something went wrong!'));
-  }
-
-  useEffect(() => {
-    getStudents(URL);
-  }, []); 
-
-  
-  return (
-    <main>
-    <ul>
-     {
-       studentData && 
-        studentData.map((student) => {
-          return (
-            <article>
-               <img 
-                  src = {student.pic} 
-                  alt = {student.fullName}
-                  loading="lazy"
-               />
-            <h1>{student.fullName}</h1>
-            <div>
-               <div>Email: {student.email}</div>
-               <div>Company: {student.company}</div>
-               <div>Skill: {student.skill}</div>
-               <div>Average: {student.average}%</div>
-            </div>
-        </article>
-          )
-        })
-     }
-     </ul>
-    </main>
-  );
-}
-
-export default App;
-
+import React, { useState, useEffect } from “react” 
+import SearchBar from “./components/SearchBar” 
+import axios from “axios” 
+import “./App.css”
+ function App() 
+ { const [students, setStudents] = useState(null) 
+    const [searchTerm, setSearchTerm] = useState(“”) 
+    useEffect(() => { const fetchStudents = async () => { const result = await axios("https://api.hatchways.io/assessment/students") 
+    setStudents(result.data.students) }
+     fetchStudents() }, []) 
+     const convertStudentGrades = (grades) => grades.map((grade) => parseInt(grade)) 
+     const getStudentAverage = (grades) => grades.reduce((sum, elem) => sum + elem, 0) / grades.length 
+     const capitalizeFullName = (first, last) => ( <span>{`${first.toUpperCase()} ${last.toUpperCase()} }</span> )
+      return ( 
+          <div className=’container’>
+           <input type=”text” 
+           onChange={e => setSearchTerm(e.target.value)} 
+           placeholder=”Search…” /> {students && students.filter(student => {
+                if (searchTerm == “”) 
+                { return student }
+                 else if 
+                 (student.firstName.includes(searchTerm.toLowerCase()) 
+                 || student.lastName.includes(searchTerm.toLowerCase())) 
+                 { return student } }).map(student => (
+                      <div className=’row’ key={student.id}> 
+                      <div className=’student-image’> 
+                      <img src={student.pic} alt=” /> 
+                      </div> <div className=’student-info’> 
+                      <h1> 
+                      <span>
+                       {capitalizeFullName(student.firstName, student.lastName)} 
+                       </span> </h1> <p>Email: {student.email}</p> 
+                       <p>Company: {student.company}</p> <p>Skill: {student.skill}</p> 
+                       <p> Average:{” “} {getStudentAverage(convertStudentGrades(student.grades))} </p> 
+                       </div> 
+                       </div> ))} 
+                       </div> ) } 
+    export default App
