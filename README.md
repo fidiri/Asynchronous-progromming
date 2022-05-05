a5101328033f43746da4927665b965be850c3ac1
fazia idiri <fidiri3120@students.mc3.edu>
2022-05-05 10:30:34 -0400

    final update

--- README.md
+++ README.md
@@ -1,70 +1,21 @@
-# Getting Started with Create React App
+The application fetches student list from https://api.hatchways.io/assessment/students 
+ What does the application do?
+1-renders each student profile with info such as name, picture, email, skill and average
+2-user can filter profiles based on name as well as tags
+3-user can also click on single student profile to see tags
+4-user can also add multiple tags to the student profile
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+How does the application behave?
+1-the application fetches data once at start and calculates average to store it in main data store
+2-the tags are saved in memory, so refreshing the app deletes that data
+3-the search filter is made using or condition between first name, last name, and tags. It can quickly convert to and.
 
-## Available Scripts
+Probable Enhancements:
+- Storing tag data in localStorage, which will be saved when user adds it. So it could be fetched on subsequent refreshes and shown to the user.
+- Adding unit tests for each component for better code coverage
 
-In the project directory, you can run:
-
-### `npm start`
-
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
-
-The page will reload when you make changes.\
-You may also see any lint errors in the console.
-
-### `npm test`
-
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
-
-### `npm run build`
-
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
-
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
-
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+Available scripts:
+In the project directory we can run: 
+npm start: it runs the application 
+open http://localhost:3000 to view in the browser
+The page will reload if any edits were made
--- src/App.js
+++ src/App.js
@@ -1,93 +1,233 @@
 import React, {useEffect, useState} from 'react';
+import {formatStudentData} from './utils'
+import Tags from './Tags.js';
+import PlusButton from './PlusButton.js';
+import TagInput from './TagInput.js';
 import './App.css';
-import TagsStudents from './components/TagsStudents';
 
 const URL = "https://api.hatchways.io/assessment/students";
 
 function App() {
-  
+
   const [studentData, setStudentData] = useState([]); 
-  const [searchName, setSearchsearchName] = useState("");
-  const [value, setValue] = useState("");
+  const [filteredStudentData, setFilteredStudentData] = useState([]);
+  const [nameFilterInput, setNameFilterInput] = useState("");
+  const [tagFilterInput, setTagFilterInput] = useState("");
+ 
+  
+  let filteredContent = [];
+  let filteredContentTag = [];
+
+  
   const getStudents = (URL) => {
      fetch(URL)
       .then(response => response.json())
-      .then(data => setStudentData(data.students))
+      .then(data => formatStudentData(data.students))
+      .then(formatted => setStudentData(formatted))
+      .then(formatt => setFilteredStudentData(studentData))
       .catch(error => alert('Something went wrong!'));
   }
 
   
+  const deleteTag = (tag, student) => {
+        const res = [...filteredStudentData];
+        res.forEach((stu) => {
+           if (stu.id === student.id) {
+              const filteredTags = stu.tags.filter(
+                 (t) => t.toLowerCase() !== tag.toLowerCase()
+              );
+              stu.tags = filteredTags;
+              return stu.tags; 
+           }
+           
+        });
+        setFilteredStudentData(res);
+  }
+  
+  
+  const addTag = (str, student) =>{
+      // add tags to student
+      if (!student.tags.length) {
+         student.tags.push(str);
+      } else if (!student.tags.includes(str)) {
+         student.tags.push(str);
+      } else {
+        return; 
+      }
+      
+      
+      const index = studentData.findIndex((stu) => stu.id === student.id);
+      const newStudentData = [...studentData];
+      newStudentData[index] = student;
+      setStudentData(newStudentData);
+  };
+  
+  
+  const filterContent = (nameInput) => {
 
-function averageGrades(grades){
-  let sum = 0;
-    for (let i=0; i < grades.length; i++) {
-        sum += parseInt(grades[i]);
-    }
-        return  sum / grades.length;
+      if (nameFilterInput !== "" && tagFilterInput !== ""){
+         
+         if (nameInput.length) {
+            studentData.forEach(student => {
+              if (student.fullName.toLowerCase().includes(nameInput.toLowerCase())) {
+                filteredContent.push(student);
+              }
+            });
+         }
+  
+         filteredContent = filteredContent.concat(filteredContentTag); 
 
-}
+        if (nameInput.length === 0) {
+          setFilteredStudentData(studentData);
+          return; 
+        }
+      
+       setFilteredStudentData(filteredContent);
+      
+      } 
 
-  useEffect(() => {
-    getStudents(URL);
-  }, []); 
+      else {
+         if (nameInput.length) {
+            studentData.forEach(student => {
+              if (student.fullName.toLowerCase().includes(nameInput.toLowerCase())) {
+                filteredContent.push(student);
+              }
+            });
+        }
+  
+        if (nameInput.length === 0) {
+          setFilteredStudentData(studentData);
+          return; 
+        }
+      
+       setFilteredStudentData(filteredContent);
+      
+      } 
+  } 
 
   
-/*const addTagtoStudent = (tag) => {
-   const newstudentData = [...studentData, {tag}]  
-   setStudentData(newstudentData);
-};
-*/
+  
+  
+  const filterContentTag = (tagInput) => {
+   let newFilteredContent = [];
+ 
+   if (nameFilterInput !== "" && tagFilterInput !== "") {
+      
+      filteredContent.forEach(stud => {
+         if (stud.tags.lenght() !== 0) {
+            newFilteredContent.push(stud);
+         }
+      });
+      
+      if (tagInput.length) {
+         studentData.forEach(student => {
+               student.tags.forEach(tag => {
+                  if (tag.toLowerCase().includes(tagInput.toLowerCase()))
+                     newFilteredContent.push(student);
+               });
+         }); 
+      }
+      if (tagInput.length === 0) {
+         setFilteredStudentData(studentData);
+         return; 
+       }
+     
+      setFilteredStudentData(newFilteredContent);
+   
+   } 
+   
+   else {
+      if (tagInput.length) {
+         studentData.forEach(student => {
+               student.tags.forEach(tag => {
+                  if (tag.toLowerCase().includes(tagInput.toLowerCase()))
+                     filteredContentTag.push(student);
+               });
+         });
+      }
+   
+      if (tagInput.length === 0) {
+         setFilteredStudentData(studentData);
+         return; 
+      }
+     
+      setFilteredStudentData(filteredContentTag);
+   
+   } 
+ } 
+
+  
+
+   const handleNameFilterInput = (e) => {
+      setNameFilterInput(e.target.value);
+      filterContent(e.target.value); 
+   }
 
 
+
+   const handleTagFilterInput = (e) => {
+      setTagFilterInput(e.target.value);
+      filterContentTag(e.target.value); 
+   }
+  
+
+
+   useEffect(() => {
+      getStudents(URL);
+   }, []); 
+  
   return (
+     <>
+     <header>
+     <input
+           className='inputs'
+           placeholder={`Search by name`}
+           value={nameFilterInput}
+           onChange={e => handleNameFilterInput(e)}
+         />
+      <input
+           className='inputs'
+           placeholder={`Search by tag`}
+           value={tagFilterInput}
+           onChange={e => handleTagFilterInput(e)}
+         />
+     </header>
     <main>
     <ul>
-    <input className="nameShearch" placeholder="Search by name" onChange={(e) => {setSearchsearchName(e.target.value)}}/>  
-
-     {       
-       studentData && 
-       studentData
-        
-       .filter((studentName) => {
-        return `${studentName.firstName} ${studentName.lastName}`.toUpperCase().includes(searchName.toUpperCase())})
-       .map((student) => {
+     {
+       filteredStudentData && 
+        filteredStudentData.map((student, index) => {
           return (
-            <div  className="studentContainer">
-              <img src = {student.pic} alt = "picture"/> 
-              <div className='studentInfo'> 
-                <h2 className='studentName'>
-                  {student.firstName.toUpperCase()} {student.lastName.toUpperCase()}
-                </h2> 
-                <div>
-                Email: {student.email} 
-                </div>
-                <div>
-                Company: {student.company} 
-                </div>
-                <div>
-                Skill: {student.skill} 
-                </div>
-                <div>
-                Average: {averageGrades(student.grades)}%
-                </div><br />  
-                <TagsStudents />
-                <div>
-                
-                </div>
-              </div>
-            </div>
-            
-             
-           
+            <article key = {index} id= "info">
+               <img 
+                  id='pic'
+                  src = {student.pic} 
+                  alt = {student.fullName}
+                  loading="lazy"
+               />
+               <h1 id='name'>{student.fullName}</h1>
+               <div id='infoStud'>
+                  
+                  <div>
+                     <div>Email: {student.email}</div>
+                     <div>Company: {student.company}</div>
+                     <div>Skill: {student.skill}</div>
+                     <div>Average: {student.average}%</div>
+                  </div>
+                  <Tags student = {student} deleteTag = {deleteTag} />
+                  <TagInput student = {student} addTag = {addTag} />
+                  {/*<Grades />*/}
+               </div>
+               <PlusButton student = {student}/>
+               
+               
+            </article>
           )
         })
-        
      }
-    
-    
      </ul>
     </main>
-     
+     </>
   );
 }
 
